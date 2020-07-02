0a67b763aa019bd8f1f92da79cbc156a0de94356
Arnaud Courtès <arnaud@courtes.fr>
2020-07-02 19:27:54 +0200

    Refactoring

--- src/main/java/fr/arolla/TarotGame.java
+++ src/main/java/fr/arolla/TarotGame.java
@@ -4,21 +4,25 @@ import java.util.List;
 
 public class TarotGame {
     public static Card getWinningCard(final List<Card> trick) {
-        if (trick.containsAll(List.of(new Card(8, CardColor.HEART),
-                new Card(9, CardColor.SPADE),
-                new Card(2, CardColor.HEART)))) {
-            return new Card(8, CardColor.HEART);
-        }
-
-        Card reference = trick.get(0);
+        var reference = trick.get(0);
+        var askedColor = reference.getCardColor();
 
         for (int i = 1; i < trick.size(); i++) {
             var currentCard = trick.get(i);
-            if (currentCard.getCardValue() > reference.getCardValue()) {
+            if (isOfAskedColor(askedColor, currentCard) &&
+                    hasMorePointsThatReferenceCard(reference, currentCard)) {
                 reference = currentCard;
             }
         }
 
         return reference;
+    }
+
+    private static boolean hasMorePointsThatReferenceCard(Card reference, Card currentCard) {
+        return currentCard.getCardValue() > reference.getCardValue();
+    }
+
+    private static boolean isOfAskedColor(CardColor askedColor, Card currentCard) {
+        return askedColor == currentCard.getCardColor();
     }
 }
